ee7266085782b17a24169430a68ad704362e7c84
Daniel <danielryz4321@gmail.com>
2025-03-19 20:18:27 +0100

    ADD: Utworzenie podstawowej struktury projektu.

---
+++ tire_shop/src/main/java/org/tireshop/tire_shop/TireShopApplication.java
@@ -0,0 +1,13 @@
+package org.tireshop.tire_shop;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class TireShopApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(TireShopApplication.class, args);
+    }
+
+}
---
+++ tire_shop/src/test/java/org/tireshop/tire_shop/TireShopApplicationTests.java
@@ -0,0 +1,13 @@
+package org.tireshop.tire_shop;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class TireShopApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
