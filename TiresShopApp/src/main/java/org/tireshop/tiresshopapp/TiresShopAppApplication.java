85e1ca1b4cfc7b269cbab05dc01f2fc18ee091df
Daniel <danielryz4321@gmail.com>
2025-04-05 15:46:22 +0200

    FEAT: Project Tires-shop init

---
+++ TiresShopApp/src/main/java/org/tireshop/tiresshopapp/TiresShopAppApplication.java
@@ -0,0 +1,13 @@
+package org.tireshop.tiresshopapp;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class TiresShopAppApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(TiresShopAppApplication.class, args);
+    }
+
+}
