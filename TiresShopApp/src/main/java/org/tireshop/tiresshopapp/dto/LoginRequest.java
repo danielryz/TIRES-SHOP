2335058a79346326da45b39f7995f31297269ab9
Daniel <danielryz4321@gmail.com>
2025-04-10 02:25:46 +0200

    FEAT: define LoginRequest DTO for login endpoint

---
+++ TiresShopApp/src/main/java/org/tireshop/tiresshopapp/dto/LoginRequest.java
@@ -0,0 +1,14 @@
+package org.tireshop.tiresshopapp.dto;
+
+import io.swagger.v3.oas.annotations.media.Schema;
+import lombok.AllArgsConstructor;
+import lombok.Data;
+
+@Data
+@AllArgsConstructor
+public class LoginRequest {
+    @Schema(example = "tires@tiresshop.pl")
+    private String email;
+    @Schema(example = "Pa$$word1")
+    private String password;
+}
