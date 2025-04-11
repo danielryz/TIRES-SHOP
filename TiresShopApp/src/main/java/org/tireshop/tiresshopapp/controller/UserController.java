16bb36258703e69a4bb2dfcc7e3ebc4938273891
Daniel <danielryz4321@gmail.com>
2025-04-11 11:08:22 +0200

    FEAT: Add new API Rest for User and edit swagger documentation

--- TiresShopApp/src/main/java/org/tireshop/tiresshopapp/controller/UserController.java
+++ TiresShopApp/src/main/java/org/tireshop/tiresshopapp/controller/UserController.java
@@ -7,11 +7,15 @@ import io.swagger.v3.oas.annotations.media.Schema;
 import io.swagger.v3.oas.annotations.responses.ApiResponse;
 import io.swagger.v3.oas.annotations.responses.ApiResponses;
 import io.swagger.v3.oas.annotations.tags.Tag;
+import jakarta.validation.Valid;
 import lombok.RequiredArgsConstructor;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.security.access.prepost.PreAuthorize;
 import org.springframework.web.bind.annotation.*;
+import org.tireshop.tiresshopapp.dto.Request.UpdateUserRequest;
+import org.tireshop.tiresshopapp.dto.Request.UpdateUserRolesRequest;
+import org.tireshop.tiresshopapp.dto.Response.UserResponse;
 import org.tireshop.tiresshopapp.entity.User;
 import org.tireshop.tiresshopapp.service.UserService;
 
@@ -27,7 +31,10 @@ public class UserController {
 
     @Operation(summary = "Pobiera wszystkich użytkowników", description = "Tylko dla administratorów")
     @ApiResponses(value = {
-            @ApiResponse(responseCode = "200", description = "Lista użytkowników zwrócona pomyślnie"),
+            @ApiResponse(responseCode = "200", description = "Lista użytkowników zwrócona pomyślnie", content = @Content(
+                    mediaType = "application/json",
+                    schema = @Schema(implementation = UserResponse.class)
+            )),
             @ApiResponse(responseCode = "400", description = "Brak uprawnień",content = @Content(
                     mediaType = "application/json",
                     examples = @ExampleObject( value = "{\"error\": \"Acces Denied\"}")
@@ -38,15 +45,15 @@ public class UserController {
     })
     @GetMapping
     @PreAuthorize("hasRole('ADMIN')")
-    public List<User> getAllUsers(){
-        return userService.getAllUsers();
+    public ResponseEntity<List<UserResponse>> getAllUsers(){
+        return ResponseEntity.ok(userService.getAllUsers());
     }
 
     @Operation(summary = "Pobiera użytkownika po ID", description = "Tylko dla administratorów")
     @ApiResponses(value = {
             @ApiResponse(responseCode = "200", description = "Użytkownik znaleziony",content = @Content(
                     mediaType = "application/json",
-                    schema = @Schema(implementation = User.class))),
+                    schema = @Schema(implementation = UserResponse.class))),
             @ApiResponse(responseCode = "400", description = "Brak uprawnień",content = @Content(
                     mediaType = "application/json",
                     examples = @ExampleObject( value = "{\"error\": \"Acces Denied\"}")
@@ -62,32 +69,124 @@ public class UserController {
     })
     @GetMapping("/{id:[0-9]+}")
     @PreAuthorize("hasRole('ADMIN')")
-    public ResponseEntity<?> getUserById(@PathVariable Long id){
+    public ResponseEntity<UserResponse> getUserById(@PathVariable Long id){
         try {
             User user = userService.getUserById(id);
-            return ResponseEntity.ok(user);
+            return ResponseEntity.ok(userService.toUserResponse(user));
         } catch (RuntimeException e) {
-            return ResponseEntity.status(HttpStatus.NOT_FOUND).body(e.getMessage());
+            return ResponseEntity.status(HttpStatus.NOT_FOUND).body(null);
         }
     }
+
     @Operation(summary = "Szczegóły zalogowanego użytkownika")
     @ApiResponses(value = {
             @ApiResponse(responseCode = "200", description = "Zwrócono dane użytkownika",content = @Content(
                     mediaType = "application/json",
-                    schema = @Schema(implementation = User.class))),
+                    schema = @Schema(implementation = UserResponse.class))),
             @ApiResponse(responseCode = "403", description = "Brak Autoryzacji", content = @Content(
                     examples = @ExampleObject(value = " ")
             ))
     })
     @GetMapping("/me")
     @PreAuthorize("hasAnyRole('USER', 'ADMIN')")
-    public ResponseEntity<?> getCurrentUser(){
-        try{
+    public ResponseEntity<UserResponse> getCurrentUser(){
             User user = userService.getCurrentUser();
-            return ResponseEntity.ok(user);
-        }catch (RuntimeException e){
-            return ResponseEntity.status(HttpStatus.NOT_FOUND).body(null);
-        }
+            return ResponseEntity.ok(userService.toUserResponse(user));
+
+    }
+
+    @Operation(summary = "Aktualizacja danych konta (username, password")
+    @ApiResponses(value = {
+            @ApiResponse(responseCode = "200", description = "Dane zostały zmienione", content = @Content(
+                    examples = @ExampleObject(value = "")
+            )),
+            @ApiResponse(responseCode = "400", description = "Nieprawidłowe dane", content = @Content(
+                    examples = @ExampleObject(value = "")
+            )),
+            @ApiResponse(responseCode = "401", description = "Brak autoryzacji", content = @Content(
+                    examples = @ExampleObject(value = "")
+            ))
+    })
+    @PatchMapping("/me")
+    @PreAuthorize("hasAnyRole('USER', 'ADMIN')")
+    public ResponseEntity<User> updateCurrentUser(@Valid @RequestBody UpdateUserRequest request){
+      return ResponseEntity.ok(userService.updateCurrentUser(request));
+    }
+
+    @Operation(summary = "Aktualizacja ról użytkownika (Admin")
+    @ApiResponses(value = {
+            @ApiResponse(responseCode = "200", description = "Rola została zmieniona", content = @Content(
+                    examples = @ExampleObject(value = "")
+            )),
+            @ApiResponse(responseCode = "404", description = "Użytkownik lub rola nie istnieje", content = @Content(
+                    examples = @ExampleObject(value = "")
+            )),
+            @ApiResponse(responseCode = "403", description = "Brak uprawnień (ADMIN)", content = @Content(
+                    examples = @ExampleObject(value = "")
+            ))
+    })
+    @PatchMapping("/{id}/role")
+    @PreAuthorize("hasRole('ADMIN')")
+    public ResponseEntity<User> updateUsersRoles(@PathVariable Long id, @RequestBody UpdateUserRolesRequest request){
+        return ResponseEntity.ok(userService.updateUserRoles(id, request.getRoles()));
+    }
+
+    @Operation(summary = "Usuwa konkretną rolę użytkownika (ADMIN)",
+    description = "Przekaż nazwe roli jako parametr zapytania: DELETE /api/users/5/role?role=ROLE_ADMIN")
+    @ApiResponses(value = {
+            @ApiResponse(responseCode = "200", description = "Rola usunięta", content = @Content(
+                    examples = @ExampleObject(value = "")
+            )),
+            @ApiResponse(responseCode = "404", description = "Użytkownik lub rola nie istnieje", content = @Content(
+                    examples = @ExampleObject(value = "")
+            )),
+            @ApiResponse(responseCode = "403", description = "Brak uprawnień", content = @Content(
+                    examples = @ExampleObject(value = "")
+            ))
+    })
+    @DeleteMapping("/{id}/role")
+    @PreAuthorize("hasRole('ADMIN')")
+    public ResponseEntity<?> removeUserRole(@PathVariable Long id, @RequestParam String role){
+        userService.removeUserRole(id, role);
+        return ResponseEntity.ok("Rola " +role+ " usunięta u użytkownika: " +id);
+    }
+
+    @Operation(summary = "Usuwa konkretnego użytkownika (ADMIN)",
+            description = "Usunięcie jednego użytkownika o id")
+    @ApiResponses(value = {
+            @ApiResponse(responseCode = "200", description = "Użytkownik usunięty", content = @Content(
+                    examples = @ExampleObject(value = "")
+            )),
+            @ApiResponse(responseCode = "404", description = "Użytkownik nie istnieje", content = @Content(
+                    examples = @ExampleObject(value = "")
+            )),
+            @ApiResponse(responseCode = "403", description = "Brak uprawnień", content = @Content(
+                    examples = @ExampleObject(value = "")
+            ))
+    })
+    @DeleteMapping("/{id}")
+    @PreAuthorize("hasRole('ADMIN')")
+    public ResponseEntity<?> deleteUserById(@PathVariable Long id){
+        userService.deleteUserById(id);
+        return ResponseEntity.ok("Użytkownik o ID "+id+" został usunięty");
+
+    }
+
+    @Operation(summary = "Usunięcie konta",
+            description = "Usunięcie swojego konta")
+    @ApiResponses(value = {
+            @ApiResponse(responseCode = "200", description = "Konto zostało usunięte", content = @Content(
+                    examples = @ExampleObject(value = "")
+            )),
+            @ApiResponse(responseCode = "401", description = "Brak autoryzacji", content = @Content(
+                    examples = @ExampleObject(value = "")
+            )),
+    })
+    @DeleteMapping("/me")
+    @PreAuthorize("hasRole('ADMIN')")
+    public ResponseEntity<?> deleteCurrentUser(){
+        userService.deleteCurrentUser();
+        return ResponseEntity.ok("Twoje konto zostało usunięte");
     }
 
 }
--- TiresShopApp/src/main/java/org/tireshop/tiresshopapp/service/UserService.java
+++ TiresShopApp/src/main/java/org/tireshop/tiresshopapp/service/UserService.java
@@ -1,23 +1,32 @@
 package org.tireshop.tiresshopapp.service;
 
 import lombok.RequiredArgsConstructor;
+import org.springframework.security.core.Authentication;
 import org.springframework.security.core.context.SecurityContextHolder;
 import org.springframework.security.core.userdetails.UserDetails;
+import org.springframework.security.crypto.password.PasswordEncoder;
 import org.springframework.stereotype.Service;
+import org.tireshop.tiresshopapp.dto.Request.UpdateUserRequest;
+import org.tireshop.tiresshopapp.dto.Response.UserResponse;
+import org.tireshop.tiresshopapp.entity.Role;
 import org.tireshop.tiresshopapp.entity.User;
+import org.tireshop.tiresshopapp.repository.RoleRepository;
 import org.tireshop.tiresshopapp.repository.UserRepository;
 
 import java.util.List;
-import java.util.Optional;
+import java.util.Set;
+import java.util.stream.Collectors;
 
 @Service
 @RequiredArgsConstructor
 public class UserService {
 
     private final UserRepository userRepository;
+    private final PasswordEncoder passwordEncoder;
+    private final RoleRepository roleRepository;
 
-    public List<User> getAllUsers(){
-        return userRepository.findAll();
+    public List<UserResponse> getAllUsers(){
+        return userRepository.findAll().stream().map(this::toUserResponse).toList();
     }
 
     public User getUserById(Long id){
@@ -39,5 +48,74 @@ public class UserService {
                 .orElseThrow(()-> new RuntimeException("Nie znaleziono zalogowanego użytkownika"));
     }
 
+    public User updateCurrentUser(UpdateUserRequest request){
+        Authentication authentication = SecurityContextHolder.getContext().getAuthentication();
+        String email = authentication.getName();
+        User user = userRepository.findByEmail(email)
+                .orElseThrow(() -> new RuntimeException("Nie znaleziono użytkownika"));
+
+        if(request.getUsername() != null && !request.getUsername().isBlank()) {
+            user.setUsername(request.getUsername());
+        }
+
+        if(request.getPassword() != null && !request.getPassword().isBlank()) {
+            user.setPassword(passwordEncoder.encode(request.getPassword()));
+        }
+        return userRepository.save(user);
+    }
+
+    public User updateUserRoles(Long userId, Set<String> roleNames){
+        User user = userRepository.findById(userId)
+                .orElseThrow(() -> new RuntimeException("Użytkownik o ID: " + userId + " nie istnieje"));
+
+        Set<Role> roles = roleNames.stream()
+                .map(roleName -> roleRepository.findByName(roleName)
+                        .orElseThrow(()->new RuntimeException("Rola nie istnieje: " + roleName)))
+                .collect(Collectors.toSet());
+
+        user.setRoles(roles);
+        return userRepository.save(user);
+    }
+
+    public User removeUserRole(Long userId, String roleName){
+        User user  = userRepository.findById(userId)
+                .orElseThrow(()->new RuntimeException("Użytkownik o ID: " + userId + " nie istnieje"));
+
+        Role role = roleRepository.findByName(roleName)
+                .orElseThrow(()->new RuntimeException("Rola nie istnieje: " + roleName));
+
+        user.getRoles().remove(role);
+        return userRepository.save(user);
+    }
+
+    public void deleteUserById(Long id){
+        if(!userRepository.existsById(id)){
+            throw new RuntimeException("Użytkownik o ID: " + id + " nie istnieje");
+        }
+        userRepository.deleteById(id);
+    }
+
+    public void deleteCurrentUser(){
+        String email = SecurityContextHolder.getContext().getAuthentication().getName();
+        User user = userRepository.findByEmail(email)
+                .orElseThrow(()->new RuntimeException("Nie znaleziono konta"));
+
+        userRepository.delete(user);
+    }
+
+    public UserResponse toUserResponse(User user){
+
+        List<String> roleNames = user.getRoles().stream().map(Role::getName).toList();
+
+        return new UserResponse(
+                user.getId(),
+                user.getUsername(),
+                user.getEmail(),
+                user.getFirstName(),
+                user.getLastName(),
+                user.getPhoneNumber(),
+                roleNames
+        );
+    }
 
 }
