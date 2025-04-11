fad6ead0c1e8e8107efa8b78a3410d086c42661d
Daniel <danielryz4321@gmail.com>
2025-04-11 17:18:23 +0200

    FEAT: Add to handle the Role entity

---
+++ TiresShopApp/src/main/java/org/tireshop/tiresshopapp/controller/RoleController.java
@@ -0,0 +1,123 @@
+package org.tireshop.tiresshopapp.controller;
+
+import io.swagger.v3.oas.annotations.Operation;
+import io.swagger.v3.oas.annotations.media.Content;
+import io.swagger.v3.oas.annotations.media.ExampleObject;
+import io.swagger.v3.oas.annotations.media.Schema;
+import io.swagger.v3.oas.annotations.responses.ApiResponse;
+import io.swagger.v3.oas.annotations.responses.ApiResponses;
+import io.swagger.v3.oas.annotations.tags.Tag;
+import lombok.RequiredArgsConstructor;
+import org.springframework.http.ResponseEntity;
+import org.springframework.security.access.prepost.PreAuthorize;
+import org.springframework.web.bind.annotation.*;
+import org.tireshop.tiresshopapp.dto.request.UpdateRoleRequest;
+import org.tireshop.tiresshopapp.entity.Role;
+import org.tireshop.tiresshopapp.service.RoleService;
+
+import java.util.List;
+
+@RestController
+@RequestMapping("/api/role")
+@RequiredArgsConstructor
+@Tag(name = "Role", description = "Obsługa encji rola, pobieranie, dodawnanie, usuwanie, update.")
+public class RoleController {
+    private final RoleService roleService;
+
+    @Operation(summary = "Pobiera wszystkie role", description = "Tylko dla administratorów")
+    @ApiResponses(value = {
+            @ApiResponse(responseCode = "200", description = "Lista ról zwrócona pomyślnie", content = @Content(
+                    mediaType = "application/json",
+                    schema = @Schema(implementation = Role.class)
+            )),
+            @ApiResponse(responseCode = "400", description = "Brak uprawnień",content = @Content(
+                    mediaType = "application/json",
+                    examples = @ExampleObject( value = "{\"error\": \"Acces Denied\"}")
+            )),
+            @ApiResponse(responseCode = "403", description = "Brak Autoryzacji", content = @Content(
+                    examples = @ExampleObject(value = " ")
+            ))
+    })
+    @GetMapping
+    @PreAuthorize("hasRole('ADMIN')")
+    public ResponseEntity<List<Role>> getAllRoles() {
+        return ResponseEntity.ok(roleService.getAllRoles());
+    }
+
+    @Operation(summary = "Pobiera Role po ID", description = "Tylko dla administratorów")
+    @ApiResponses(value = {
+            @ApiResponse(responseCode = "200", description = "Rola znaleziona",content = @Content(
+                    mediaType = "application/json",
+                    schema = @Schema(implementation = Role.class))),
+            @ApiResponse(responseCode = "400", description = "Brak uprawnień",content = @Content(
+                    mediaType = "application/json",
+                    examples = @ExampleObject( value = "{\"error\": \"Acces Denied\"}")
+            )),
+            @ApiResponse(responseCode = "403", description = "Brak Autoryzacji", content = @Content(
+                    examples = @ExampleObject(value = " ")
+            )),
+            @ApiResponse(responseCode = "404", description = "Rola nie istnieje", content = @Content(
+                    examples = @ExampleObject()
+            ))
+    })
+    @GetMapping("/{id}")
+    public ResponseEntity<Role> getRoleById(@PathVariable Long id) {
+        return ResponseEntity.ok(roleService.getRoleById(id));
+    }
+
+    @Operation(summary = "Dodaje nową rolę", description = "Tylko dla administratorów")
+    @ApiResponses(value = {
+            @ApiResponse(responseCode = "200", description = "Rola dodana"),
+            @ApiResponse(responseCode = "400", description = "Brak uprawnień",content = @Content(
+                    mediaType = "application/json",
+                    examples = @ExampleObject( value = "{\"error\": \"Acces Denied\"}")
+            )),
+            @ApiResponse(responseCode = "403", description = "Brak Autoryzacji", content = @Content(
+                    examples = @ExampleObject(value = " ")
+            ))
+    })
+    @PostMapping
+    @PreAuthorize("hasRole('ADMIN')")
+    public ResponseEntity<Role> createRole(@RequestBody String name) {
+        return ResponseEntity.ok(roleService.createRole(name));
+    }
+
+    @Operation(summary = "Update roli", description = "Tylko dla administratorów")
+    @ApiResponses(value = {
+            @ApiResponse(responseCode = "200", description = "Rola zmieniona"),
+            @ApiResponse(responseCode = "400", description = "Brak uprawnień",content = @Content(
+                    mediaType = "application/json",
+                    examples = @ExampleObject( value = "{\"error\": \"Acces Denied\"}")
+            )),
+            @ApiResponse(responseCode = "403", description = "Brak Autoryzacji", content = @Content(
+                    examples = @ExampleObject(value = " ")
+            ))
+    })
+    @PatchMapping("/{id}")
+    @PreAuthorize("hasRole('ADMIN')")
+    public ResponseEntity<Role> updateRole(@PathVariable Long id, @RequestBody UpdateRoleRequest request) {
+        Role updated = roleService.updateRole(id, request.name());
+        return ResponseEntity.ok(updated);
+    }
+
+
+    @Operation(summary = "Usunięcie roli", description = "Tylko dla administratorów")
+    @ApiResponses(value = {
+            @ApiResponse(responseCode = "200", description = "Rola usunięta"),
+            @ApiResponse(responseCode = "400", description = "Brak uprawnień",content = @Content(
+                    mediaType = "application/json",
+                    examples = @ExampleObject( value = "{\"error\": \"Acces Denied\"}")
+            )),
+            @ApiResponse(responseCode = "403", description = "Brak Autoryzacji", content = @Content(
+                    examples = @ExampleObject(value = " ")
+            ))
+    })
+    @DeleteMapping
+    @PreAuthorize("hasRole('ADMIN')")
+    public ResponseEntity<?> deleteRole(@RequestBody UpdateRoleRequest request){
+        roleService.deleteRole(request.name());
+        return ResponseEntity.ok("Rola " + request.name() + " usunięta");
+    }
+
+
+}
---
+++ TiresShopApp/src/main/java/org/tireshop/tiresshopapp/service/RoleService.java
@@ -0,0 +1,53 @@
+package org.tireshop.tiresshopapp.service;
+
+import lombok.RequiredArgsConstructor;
+import org.springframework.stereotype.Service;
+import org.tireshop.tiresshopapp.entity.Role;
+import org.tireshop.tiresshopapp.repository.RoleRepository;
+
+import java.util.List;
+
+@Service
+@RequiredArgsConstructor
+public class RoleService {
+    private final RoleRepository roleRepository;
+
+    public List<Role> getAllRoles() {
+        return roleRepository.findAll();
+    }
+
+    public Role getRoleById(Long id) {
+        return roleRepository.findById(id)
+                .orElseThrow(() -> new RuntimeException("Rola o id " + id + " nie istnieje"));
+    }
+
+    public Role createRole(String name) {
+        if (roleRepository.findByName(name).isPresent()) {
+            throw new RuntimeException("Rola " + name + " już istnieje");
+        }
+        Role role = new Role();
+        role.setName(name);
+        return roleRepository.save(role);
+    }
+
+    public Role updateRole(Long id, String newName) {
+        Role role = roleRepository.findById(id)
+                .orElseThrow(() -> new RuntimeException("Rola o id " + id + " nie istnieje"));
+
+        if (roleRepository.findByName(newName).isPresent()) {
+            throw new RuntimeException("Rola " +newName+ "już istnieje");
+        }
+        role.setName(newName);
+        return roleRepository.save(role);
+    }
+
+    public void deleteRole(String name){
+        Role role = roleRepository.findByName(name)
+                .orElseThrow(() -> new RuntimeException("Rola " + name + " nie istnieje"));
+
+        if (!role.getUsers().isEmpty()) {
+            throw new RuntimeException("Nie można usunąć Roli przypisanej do użytkownika");
+        }
+        roleRepository.delete(role);
+    }
+}
