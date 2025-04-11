1ab78680efbd4e10b095b584521f02fdbed7ae11
Daniel <danielryz4321@gmail.com>
2025-04-11 11:13:34 +0200

    FEAT: UPDATE

--- TiresShopApp/src/main/java/org/tireshop/tiresshopapp/entity/User.java
+++ TiresShopApp/src/main/java/org/tireshop/tiresshopapp/entity/User.java
@@ -1,6 +1,5 @@
 package org.tireshop.tiresshopapp.entity;
 
-import com.fasterxml.jackson.annotation.JsonIgnore;
 import io.swagger.v3.oas.annotations.media.Schema;
 import jakarta.persistence.*;
 import lombok.*;
